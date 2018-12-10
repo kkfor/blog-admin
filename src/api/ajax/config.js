2aa1bcf4c8cdb7374457d854f1edafad1bd29ef3
wyq-mac <wangyaqi.chn@gmail.com>
2018-12-10 23:22:18 +0800

    https

--- src/api/ajax/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  baseURL: 'http://localhost:4000/api'
-}
-
-export default config
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   baseURL: process.env.NODE_ENV === 'development' ?
     '//localhost:5000/api' :
-    '//118.24.148.253:5000/api',
+    '//api.kkfor.com/api',
   staticURL: '//static.kkfor.com',
-  upLoadImg: 'http://upload-z1.qiniup.com'
+  upLoadImg: '//upload-z1.qiniup.com'
 }
