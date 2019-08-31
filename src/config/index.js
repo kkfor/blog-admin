ddd9bfef08a6838b27d4ac89a2586bb6a78adc19
wyq-mac <wangyaqi.chn@gmail.com>
2019-08-31 22:25:53 +0800

    update: baseurl

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   baseURL: process.env.NODE_ENV === 'development' ?
     '//localhost:5000' :
-    '//api.kkfor.com/api',
+    '//api.kkfor.com',
   staticURL: '//static.kkfor.com',
   upLoadImg: '//upload-z1.qiniup.com'
 }
