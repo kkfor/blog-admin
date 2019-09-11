134e5a6c1308b9c6a7c80b1eb56ada17a19b05e7
wangyaqi <wangyaqi.chn@foxmail.com>
2019-09-11 18:30:56 +0800

    update: menu

--- src/layout/container/index.jsx
+++ src/layout/container/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import styles from './index.module.scss'
 import { Route, Link, Switch } from 'react-router-dom'
 import { route, menu } from '../../router'
@@ -28,9 +28,17 @@ class Container extends Component {
         <aside className={styles.aside}>
           <ul>
             {menu.map((item, index) => (
-              <li key={index} className={item.path === pathname ? styles.active : ''}>
+              <li
+                key={index}
+                className={item.path === pathname ? styles.active : ''}
+              >
                 <div>
-                  <Link to={item.path} className={item.path === pathname ? styles.active : ''}>{item.name}</Link>
+                  <Link
+                    to={item.path}
+                    className={item.path === pathname ? styles.active : ''}
+                  >
+                    {item.name}
+                  </Link>
                 </div>
                 {this.renderChildren(item)}
               </li>
@@ -49,17 +57,19 @@ class Container extends Component {
   }
 
   renderChildren(item) {
-    const { pathname } = this.state
+    const pathname = history.location.pathname
 
     if (item.children && item.children.length) {
       return (
         <ul>
           {item.children.map((subitem, subindex) => (
-            <li
-              key={subindex}
-              className={subitem.path === pathname ? styles.active : ''}
-            >
-              <Link to={subitem.path}>{subitem.name}</Link>
+            <li key={subindex}>
+              <Link
+                className={subitem.path === pathname ? styles.active : ''}
+                to={subitem.path}
+              >
+                {subitem.name}
+              </Link>
             </li>
           ))}
         </ul>
