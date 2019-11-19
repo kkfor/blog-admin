b3aa77525f54073c1aa1b85f92cea18aeaf9c24c
wangyaqi <wangyaqi.chn@foxmail.com>
2019-11-19 17:35:53 +0800

    add: category

--- src/views/category/index.jsx
+++ src/views/category/index.jsx
@@ -1,8 +1,8 @@
-import React, { Component, Fragment } from "react"
-import { Table, Button, Modal, Input, Form } from "antd"
-import api from "@/api"
-import styles from "./index.module.scss"
-import history from "@/config/history"
+import React, { Component, Fragment } from 'react'
+import { Table, Button, Modal, Input, Form } from 'antd'
+import api from '@/api'
+import styles from './index.module.scss'
+import history from '@/config/history'
 
 const FormItem = Form.Item
 
@@ -10,7 +10,12 @@ class Category extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      id: null,
+      name: null,
+      slug: null,
+      url: null,
       category: [],
+      editStatus: false, // false: 新增状态 | true: 编辑状态
       table: {
         pagination: false
       }
@@ -32,19 +37,49 @@ class Category extends Component {
     }
   }
 
-  edit(id) {
-    const url = "/category/edit/" + id
-    history.push(url)
+  async edit(id) {
+    try {
+      const res = await api.category.getCategory(id)
+      this.setState(
+        {
+          editStatus: true,
+          id,
+          name: res.name,
+          slug: res.slug,
+          url: res.url
+        },
+        () => {
+          this.props.form.resetFields()
+        }
+      )
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  addCategory() {
+    this.setState(
+      {
+        id: null,
+        name: null,
+        slug: null,
+        url: null,
+        editStatus: false
+      },
+      () => {
+        this.props.form.resetFields()
+      }
+    )
   }
 
   delete(id) {
     let that = this
     Modal.confirm({
-      title: "提示?",
-      content: "确定要删除吗",
-      okText: "确定",
-      okType: "danger",
-      cancelText: "取消",
+      title: '提示?',
+      content: '确定要删除吗',
+      okText: '确定',
+      okType: 'danger',
+      cancelText: '取消',
       async onOk() {
         await api.category.delCategory(id)
         that.init()
@@ -63,6 +98,7 @@ class Category extends Component {
           } else {
             await api.category.postCategory(values)
           }
+          this.init()
         } catch (err) {
           console.error(err)
         }
@@ -72,28 +108,33 @@ class Category extends Component {
 
   render() {
     const { getFieldDecorator } = this.props.form
-    const { name, slug, url } = this.state
+    const { name, slug, url, editStatus } = this.state
 
     let dataSource = this.state.category
 
     const columns = [
       {
-        title: "分类名",
-        dataIndex: "slug",
-        key: "slug"
+        title: '分类名',
+        dataIndex: 'name',
+        key: 'name'
       },
       {
-        title: "url",
-        dataIndex: "url",
-        key: "url"
+        title: '分类别名',
+        dataIndex: 'slug',
+        key: 'slug'
       },
       {
-        title: "操作",
-        dataIndex: "_id",
-        key: "_id",
+        title: 'url',
+        dataIndex: 'url',
+        key: 'url'
+      },
+      {
+        title: '操作',
+        dataIndex: '_id',
+        key: '_id',
         render: id => (
           <Fragment>
-            <Button onClick={this.edit.bind(this, id)}>编辑</Button>
+            <Button onClick={this.edit.bind(this, id)}>编辑</Button>{' '}
             <Button onClick={this.delete.bind(this, id)} type="danger">
               删除
             </Button>
@@ -103,28 +144,39 @@ class Category extends Component {
     ]
     return (
       <div className={styles.main}>
-        <h2>分类</h2>
+        <div className={styles.top}>
+          <h2>分类</h2>
+          {editStatus && (
+            <span onClick={() => this.addCategory()}>新增分类</span>
+          )}
+        </div>
         <div className={styles.list}>
           <div className={styles.edit}>
             <Form onSubmit={this.handleSubmit} className={styles.form}>
               <FormItem>
-                {getFieldDecorator("name", {
+                {getFieldDecorator('name', {
                   initialValue: name
                 })(<Input placeholder="输入分类名"></Input>)}
               </FormItem>
               <FormItem>
-                {getFieldDecorator("slug", {
+                {getFieldDecorator('slug', {
                   initialValue: slug
                 })(<Input placeholder="输入分类别名"></Input>)}
               </FormItem>
               <FormItem>
-                {getFieldDecorator("url", {
+                {getFieldDecorator('url', {
                   initialValue: url
                 })(<Input placeholder="输入分类url"></Input>)}
               </FormItem>
-              <Button htmlType="submit" type="primary">
-                提交
-              </Button>
+              {editStatus ? (
+                <Button htmlType="submit" type="primary">
+                  修改分类
+                </Button>
+              ) : (
+                <Button htmlType="submit" type="primary">
+                  新增分类
+                </Button>
+              )}
             </Form>
           </div>
           <Table
